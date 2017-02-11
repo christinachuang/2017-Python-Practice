b041c61b6afc6974bf04804a8891ce75db3b23fb
christinachuang <c10032327@gmail.com>
2017-02-11 12:44:21 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ import sys
 from datetime import datetime
 
 import tensorflow as tf
+import pdb
 
 '''
 1. import `Video` and `save_video` from the correct module of package "styler"
 '''
-from ... import Video
-from ... import save_video
+from styler.video import Video
+from styler.utils import save_video
 
 
 model_file = 'data/vg-30.pb'
@@ -41,7 +42,7 @@ def main():
         '''
         2. set the `path` to  your input
         '''
-        with Video(...) as v:
+        with Video('D:\Python-Practice\input\jaguar.mp4') as v:
             frames = v.read_frames(image_h=shape[1], image_w=shape[2])
 
         logging.info("Processing image")
@@ -51,17 +52,18 @@ def main():
         3. Write a list comprehension to iterate through all frames,
            and make it be processed by Tensorflow.
         '''
+        
         processed = [
-            session.run(out, feed_dict={image: [frame]})
-            ...
+            session.run(out, feed_dict={image: [frame]})  for frame in frames
         ]
+        #pdb.set_trace()
 
         '''
         4. Pass the results as a argument into function
         '''
         save_video('result.mp4',
                    fps=30, h=shape[1], w=shape[2],
-                   frames=...)
+                   frames=processed)
 
         logging.info("Processing took %f" % (
             (datetime.now() - start_time).total_seconds()))
